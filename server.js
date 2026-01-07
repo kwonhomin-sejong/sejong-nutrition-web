d32bb99fb9b832e5fd85c6be1adf17f6aaef415d
HOMIN <kwonhomin1010@gmail.com>
2026-01-07 20:48:48 +0900

    update store name to real franchise

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const path = require("path");
 const app = express();
 
 const XLSX = require("xlsx");
-const path = require("path");
 //const PORT = process.env.PORT || 3000;
 
 // ----------------------
